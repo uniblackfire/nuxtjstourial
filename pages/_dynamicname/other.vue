57f90634c39709cf70ec929e9d0e6ab82c7a9cb1
bitzx <bitzx@xxxx.com>
2020-03-05 23:56:19 +0800

    动态路由

---
+++ pages/_dynamicname/other.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    动态路由 其他页
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData (ctx) {
+    // eslint-disable-next-line no-console
+    console.log(ctx.route.params.dynamicname)
+  }
+}
+</script>
